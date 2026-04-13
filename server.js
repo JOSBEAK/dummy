bc8839b39666a9d9ab8e0da7496c31c861d8897c
Anurag Pandey <anurag@trustworthy.com>
2026-04-13 19:43:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('node:http');
+
+// Setup hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create server and handle requests
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Start the server
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
