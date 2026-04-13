cb2443092e4ed3e9a74ee2f8423880e52654747e
JOSBEAK <anuragpandey4900@gmail.com>
2026-04-13 20:15:04 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('node:http');
 
 // Setup hostname and port
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 
 // Create server and handle requests
 const server = http.createServer((req, res) => {
